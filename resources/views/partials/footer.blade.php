a9f7efc346c7c2812e13e6897a9ef6dae6e65caa
FrancescoGalanti <galanti_francesco>
2021-01-03 16:54:07 +0100

    working

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,38 +7,27 @@
   
   <div class="container-footer">
      <ul>
-        <li>
-           a
-        </li>    
-        <li>
-           a
-        </li> 
-        <li>
-           a
-        </li> 
-        <li>
-           a
-        </li> 
-        <li>
-           a
-        </li> 
-     </ul>
-     <ul>
-        <li>
-           a
-        </li>    
-        <li>
-           a
-        </li> 
-        <li>
-           a
-        </li> 
-        <li>
-           a
-        </li> 
-        <li>
-           a
-        </li> 
+       <li>
+         Ragione sociale: La Molisana S.P.A.
+       </li>
+       <li>
+         Sede legale: Contrada Colle  delle Alpi, 100/A
+       </li>
+       <li>
+         86100 - Campobasso (CB)
+       </li>
+       <li>
+         Pec: lamolisana@pec.it     
+       </li>
+       <li>
+         Tel: +39 0874 4981
+       </li>
+       <li>
+         Fax: +39 0874 629584
+       </li>
+       <li>
+         info@lamolisana.it (per segnalazioni degli utenti)
+       </li>
      </ul>
   </div>
 </footer>
